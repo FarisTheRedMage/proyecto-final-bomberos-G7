78e33a5802de889970e305945deea3a5d128639b
Nicolás Fernández <84243912+FarisTheRedMage@users.noreply.github.com>
2023-10-13 19:25:19 -0300

    Commit 50 - Conexion de interface al Main

--- JavaApplication1/src/bomberosApp/AccesoADatos/Main.java
+++ JavaApplication1/src/bomberosApp/AccesoADatos/Main.java
@@ -4,17 +4,18 @@ import bomberosApp.Entidades.Bombero;
 import bomberosApp.Entidades.Brigada;
 import bomberosApp.Entidades.Cuartel;
 import bomberosApp.Entidades.Siniestro;
+import bomberosApp.Vistas.disenioGestionBomberosApp;
 import java.time.LocalDate;
 import java.util.List;
+import javax.swing.JFrame;
 
 public class Main {
 
     public static void main(String[] args) {
         
-        
-        
-        
-        
-        
+          disenioGestionBomberosApp miapp=new disenioGestionBomberosApp();
+          miapp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          miapp.setLocationRelativeTo(null);
+          miapp.setVisible(true);
     }
 }
